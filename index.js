64fe5146481689c0522c1cd22417e5919fb28219
DiamonC <88857034+DiamonC@users.noreply.github.com>
2022-10-05 15:43:12 -0500

    t

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // importing packages
-const express = require('express');
+const express = require("express");
 const app = express();
-const hello = require('./routes/hello');
+const hello = require("./routes/hello");
 // middlewares
 app.use(express.json());
 // adding routes
-app.use('/hello', hello);
+app.use("/hello", hello);
 // port
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
