eef7175ebb194a181c4813a0dee480a70e3766f9
sveltarne <114943151+sveltarne@users.noreply.github.com>
2022-10-05 10:53:09 -0500

    r

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const express = require("express");
 const app = express();
 const hello = require("./routes/hello");
+const account = require("./routes/account");
 // middlewares
 app.use(express.json());
 // adding routes
 app.use("/hello", hello);
+app.use("/account", account);
 // port
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
--- routes/account.js
+++ routes/account.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 router.get(`/`, function (req, res) {
-	res.status(200).json('./storage/accounts/bob.js');
+	res.status(200).json('./storage/accounts/bob.json');
 });
 router.post(`/`, function (req, res) {
 	res.status(200).json({msg: `It's a POST request.`});
