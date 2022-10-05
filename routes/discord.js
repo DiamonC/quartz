64d2d1f12d66859143c63b724aa81188f327df04
DiamonC <88857034+DiamonC@users.noreply.github.com>
2022-10-05 16:55:15 -0500

    v

--- routes/discord.js
+++ routes/discord.js
@@ -1,14 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const redirect = encodeURIComponent('http://localhost:50451/api/discord/callback');
+const redirect = encodeURIComponent(
+  "http://localhost:50451/api/discord/callback"
+);
 
-router.get('/login', (req, res) => {
-  res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`);
+router.get("/login", (req, res) => {
+  res.redirect(
+    `https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`
+  );
 });
-
 
 module.exports = router;
