2f12ce6d1ba8de6a63be0cc53c0943fe4f373766
sveltarne <114943151+sveltarne@users.noreply.github.com>
2022-10-05 11:04:47 -0500

    r

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // importing packages
 const express = require("express");
 const app = express();
-const hello = require("./routes/hello");
 const account = require("./routes/account");
 // middlewares
 app.use(express.json());
 // adding routes
-app.use("/hello", hello);
+app.use("/hello", require("./routes/hello"));
 app.use("/account", account);
+app.use('/api/discord', require('./api/discord'));
 // port
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
