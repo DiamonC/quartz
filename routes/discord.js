afb3803379b439607634af1f3c53e04d307922bd
sveltarne <114943151+sveltarne@users.noreply.github.com>
2022-10-06 15:02:46 -0500

    added callback handler

--- routes/discord.js
+++ routes/discord.js
@@ -15,4 +15,19 @@ router.get("/login", (req, res) => {
   );
 });
 
+router.get('/callback', catchAsync(async (req, res) => {
+  if (!req.query.code) throw new Error('NoCodeProvided');
+  const code = req.query.code;
+  const creds = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+  const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${creds}`,
+      },
+    });
+  const json = await response.json();
+  res.redirect(`/?token=${json.access_token}`);
+}));
+
 module.exports = router;
