e0cfec059332f3fbceca1ae538e5d1115c7022b1
sveltarne <114943151+sveltarne@users.noreply.github.com>
2022-10-06 15:03:45 -0500

    error handler

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ app.use("/discord", require("./routes/discord"));
 // port
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
+
+
+app.use((err, req, res, next) => {
+    switch (err.message) {
+      case 'NoCodeProvided':
+        return res.status(400).send({
+          status: 'ERROR',
+          error: err.message,
+        });
+      default:
+        return res.status(500).send({
+          status: 'ERROR',
+          error: err.message,
+        });
+    }
+  });
