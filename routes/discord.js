6574e9914b1374bc18998c4430af944af210286c
DiamonC <88857034+DiamonC@users.noreply.github.com>
2022-10-06 20:02:04 -0500

    d

--- routes/discord.js
+++ routes/discord.js
@@ -5,21 +5,6 @@ const router = express.Router();
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-router.get('/callback', catchAsync(async (req, res) => {
-  if (!req.query.code) throw new Error('NoCodeProvided');
-  const code = req.query.code;
-  const creds = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
-  const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`,
-    {
-      method: 'POST',
-      headers: {
-        Authorization: `Basic ${creds}`,
-      },
-    });
-  const json = await response.json();
-  res.redirect(`/?token=${json.access_token}`);
-}));
-
 const redirect = encodeURIComponent(
   "http://localhost:4000/api/discord/callback"
 );
@@ -31,4 +16,3 @@ router.get("/login", (req, res) => {
 });
 
 module.exports = router;
-
