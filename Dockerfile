01236cca48abc21437480c232ca9c70fe321fb57
DiamonC <diamoncyt@gmail.com>
2022-10-11 16:45:54 -0500

    formatted with prettier, cleaned up dockerfile comments

--- Dockerfile
+++ Dockerfile
@@ -3,18 +3,16 @@ FROM node:18
 # Create app directory
 WORKDIR /usr/src/observer
 
-# Install app dependencies
-# A wildcard is used to ensure both package.json AND package-lock.json are copied
-# where available (npm@5+)
+# Copy package files
 COPY package*.json ./
 
+# Install packages
 RUN npm install
-# If you are building your code for production
-# RUN npm ci --only=production
 
 # Bundle app source
 COPY . .
 
 EXPOSE 4000
 
+# Run the app
 CMD [ "node", "run" ]
--- routes/discord.js
+++ routes/discord.js
@@ -1,15 +1,13 @@
-const express = require('express');
-const fetch = require('node-fetch');
-const btoa = require('btoa');
-const { catchAsync } = require('../utils');
+const express = require("express");
+const fetch = require("node-fetch");
+const btoa = require("btoa");
+const { catchAsync } = require("../utils");
 const router = express.Router();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
-const redirect = encodeURIComponent(
-  "https://api.arthmc.xyz/discord/callback/"
-);
+const redirect = encodeURIComponent("https://api.arthmc.xyz/discord/callback/");
 
 router.get("/login", (req, res) => {
   res.redirect(
@@ -17,19 +15,24 @@ router.get("/login", (req, res) => {
   );
 });
 
-router.get('/callback', catchAsync(async (req, res) => {
-  if (!req.query.code) throw new Error('NoCodeProvided');
-  const code = req.query.code;
-  const creds = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
-  const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`,
-    {
-      method: 'POST',
-      headers: {
-        Authorization: `Basic ${creds}`,
-      },
-    });
-  const json = await response.json();
-  res.redirect(`/?token=${json.access_token}`);
-}));
+router.get(
+  "/callback",
+  catchAsync(async (req, res) => {
+    if (!req.query.code) throw new Error("NoCodeProvided");
+    const code = req.query.code;
+    const creds = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+    const response = await fetch(
+      `https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${creds}`,
+        },
+      }
+    );
+    const json = await response.json();
+    res.redirect(`/?token=${json.access_token}`);
+  })
+);
 
 module.exports = router;
