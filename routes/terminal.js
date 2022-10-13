f3c20b533765c87def5f2232ae4caba80234cf36
DiamonC <88857034+DiamonC@users.noreply.github.com>
2022-10-13 13:00:22 -0500

    fixed typo

--- routes/terminal.js
+++ routes/terminal.js
@@ -1,23 +1,21 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/terminal.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/terminal.html");
 });
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
-    socket.on('disconnect', () => {
-      console.log('A user disconnectedd');
-    });
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
   });
-  
+});
 
 server.listen(4001, () => {
-  console.log('listening on *:4001');
+  console.log("listening on *:4001");
 });
