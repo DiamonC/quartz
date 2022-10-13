6ab38353817eedf768dcc9589550c0a402f4c0c9
DiamonC <88857034+DiamonC@users.noreply.github.com>
2022-10-13 14:05:29 -0500

    changed port to fix error

--- routes/terminal.js
+++ routes/terminal.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const http = require("http").Server(express);
 const io = require("socket.io")(http);
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4001;
 
 router.get("/", (req, res) => {
   res.sendFile(__dirname + "/terminal.html");
