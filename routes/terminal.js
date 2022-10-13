df705ed6e0721d84f8dc16a8bdf4213b69998e2b
DiamonC <88857034+DiamonC@users.noreply.github.com>
2022-10-13 14:30:06 -0500

    seperated terminal from rest of app

--- routes/terminal.js
+++ routes/terminal.js
@@ -2,21 +2,18 @@ const express = require("express");
 const router = express.Router();
 const http = require("http").Server(express);
 const io = require("socket.io")(http);
+const port = process.env.PORT || 4001;
 
-
-router.get("/", (req, res) => {
+router.get("/terminal", (req, res) => {
   res.sendFile(__dirname + "/terminal.html");
 });
 
 io.on("connection", (socket) => {
-  console.log('a user connected');
+  console.log("a user connected");
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
 });
-
-
-module.exports = router;
--- run.js
+++ run.js
@@ -13,7 +13,6 @@ app.use(express.json());
 
 app.use("/discord", require("./routes/discord"));
 app.use("/server", require("./routes/server"));
-app.use("/terminal", require("./routes/terminal"));
 // port
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on Port: ${port}`));
