81f450cf4b711e3f13d9360c6b42be9452347d91
sveltarne <114943151+sveltarne@users.noreply.github.com>
2022-10-13 13:09:28 -0500

    updated code

--- routes/terminal.js
+++ routes/terminal.js
@@ -1,32 +1,18 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/terminal.html");
-});
-
-io.on("connection", (socket) => {
-  console.log("A user connected");
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
-});
-
-server.listen(4001, () => {
-  console.log("listening on *:4001");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-    });
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
   });
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
