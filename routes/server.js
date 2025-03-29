cfbf5de8b62c4fc6b26d956eb735b64efe82d0bf
DiamonC <diamonc@proton.me>
2025-03-29 14:41:17 -0500

    finally fixed crash

--- routes/server.js
+++ routes/server.js
@@ -127,7 +127,12 @@ router.get(`/claim/:id`, function (req, res) {
                     freeServers = parseInt(account.freeServers);
                   }
                   console.log("account.servers: " + JSON.stringify(account));
+                  try {
                   hasPayedForServer = subs + freeServers > account.servers.length;
+                  } catch (err) {
+
+                    hasPayedForServer = false; 
+                  }
                   console.log("hasPayedForServer3: " + subs + " "+freeServers + ">" + account.servers.length);
                   if (hasPayedForServer) {
                     if (!account.servers.includes(id)) {
